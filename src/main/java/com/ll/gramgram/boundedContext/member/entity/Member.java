c837919751338dee8444afa885b358538bdc00be
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-03-30 13:33:31 +0900

    회원가입 처리

---
+++ src/main/java/com/ll/gramgram/boundedContext/member/entity/Member.java
@@ -0,0 +1,29 @@
+package com.ll.gramgram.boundedContext.member.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+    private String username;
+    private String password;
+}
